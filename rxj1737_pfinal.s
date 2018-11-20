fb5dbc3c7c537db2bb036f5c66640707c32db986
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-11-19 22:45:17 -0600

    added functionality to test on pi

--- rxj1737_pfinal.s
+++ rxj1737_pfinal.s
@@ -19,7 +19,15 @@ writeloop:
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
-    STR R2, [R2]            @ write the address of a[i] to a[i]
+	PUSH {R0}
+	PUSH {R1}
+	PUSH {R2}
+	BL _scanf
+    MOV R3, R0
+	POP {R2}
+	POP {R1}
+	POP {R0}
+	STR R3, [R2]            @ write the address of a[i] to a[i]
     ADD R0, R0, #1          @ increment index
     B   writeloop           @ branch to next loop iteration
 writedone:
@@ -43,26 +51,70 @@ readloop:
     ADD R0, R0, #1          @ increment index
     B   readloop            @ branch to next loop iteration
 readdone:
-    B _exit                 @ exit if done
+	BL _printfsearch		@get user input to search
+	BL _scanf
+	MOV R5, R0				@R5 holds the value to search
+	MOV R9, #0				@R9 Holds the boolean value of is_value_present
+	MOV R0, #0
+readloop_second:
+    CMP R0, #10            @ check to see if we are done iterating
+    BEQ read2done            @ exit loop if done
+    LDR R1, =a              @ get address of a
+    LSL R2, R0, #2          @ multiply index*4 to get array offset
+    ADD R2, R1, R2          @ R2 now has the element address
+    LDR R3, [R2]            @ read the array at address
+    PUSH {R0}               @ backup register before printf
+    PUSH {R1}               @ backup register before printf
+    PUSH {R2}               @ backup register before printf
+    MOV R3, R1              @ move array value to R2 for printf
+    MOV R1, R0              @ move array index to R1 for printf
+	COMP R3, R5
+    BLEQ  _printf             @ branch to print procedure with return
+    MOVEQ R9, #1
+	POP {R2}                @ restore register
+    POP {R1}                @ restore register
+    POP {R0}                @ restore register
+    ADD R0, R0, #1          @ increment index
+    B   readloop_second            @ branch to next loop iteration
+read2done:
+	CMP R9, #1 
+	BEQ _exit                 @ exit if done
     
 _exit:  
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str:
+    LDR R1, =notfound_str   @ string at label exit_str:
     SWI 0                   @ execute syscall
     MOV R7, #1              @ terminate syscall, 1
     SWI 0                   @ execute syscall
-       
+ 
+_scanf:
+    PUSH {LR}                @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                 @ return
+
+_printfsearch:
+    PUSH {LR}               @ store the return address
+    LDR R0, =printf_search     @ R0 contains formatted string address
+    BL printf               @ call printf
+    POP {PC}                @ restore the stack pointer and return
+	
 _printf:
     PUSH {LR}               @ store the return address
     LDR R0, =printf_str     @ R0 contains formatted string address
     BL printf               @ call printf
     POP {PC}                @ restore the stack pointer and return
-   
+
 .data
 
 .balign 4
 a:              .skip       40
-printf_str:     .asciz      "a[%d] = %d\n"
-exit_str:       .ascii      "Terminating program.\n"
+printf_str:     .asciz      "array_a[%d] = %d\n"
+notfound_str:       .ascii      "Terminating program.\n"
+printf_search:	.asciz		"ENTER A SEARCH VALUE: "
