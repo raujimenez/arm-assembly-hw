43dcc786197de83bcb1b2558ae433112cc131244
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-11-19 23:21:22 -0600

    completed implementation

--- rxj1737_pfinal.s
+++ rxj1737_pfinal.s
@@ -53,7 +53,7 @@ readloop:
 readdone:
 	BL _printfsearch		@get user input to search
 	BL _scanf
-	MOV R5, R0				@R5 holds the value to search
+	MOV R8, R0				@R8 holds the value to search
 	MOV R9, #0				@R9 Holds the boolean value of is_value_present
 	MOV R0, #0
 readloop_second:
@@ -62,33 +62,29 @@ readloop_second:
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
-    LDR R3, [R2]            @ read the array at address
+    LDR R1, [R2]            @ read the array at address
     PUSH {R0}               @ backup register before printf
     PUSH {R1}               @ backup register before printf
     PUSH {R2}               @ backup register before printf
-    MOV R3, R1              @ move array value to R2 for printf
+    MOV R2, R1              @ move array value to R2 for printf
     MOV R1, R0              @ move array index to R1 for printf
-	COMP R3, R5
+    CMP R2, R8
     BLEQ  _printf             @ branch to print procedure with return
     MOVEQ R9, #1
-	POP {R2}                @ restore register
+    POP {R2}                @ restore register
     POP {R1}                @ restore register
     POP {R0}                @ restore register
     ADD R0, R0, #1          @ increment index
     B   readloop_second            @ branch to next loop iteration
 read2done:
-	CMP R9, #1 
-	BEQ _exit                 @ exit if done
-    
+	CMP R9, #0 
+	BLEQ _printnotfound                 @ exit if done
+	B _exit
+
 _exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =notfound_str   @ string at label exit_str:
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
- 
+	MOV R7, #1
+	SWI 0
+    	     
 _scanf:
     PUSH {LR}                @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
@@ -98,6 +94,13 @@ _scanf:
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}                 @ return
+
+_printnotfound:
+    PUSH {LR}               @ store the return address
+    LDR R0, =notfound_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    POP {PC}                @ restore the stack pointer and return
+	
 
 _printfsearch:
     PUSH {LR}               @ store the return address
@@ -116,5 +119,7 @@ _printf:
 .balign 4
 a:              .skip       40
 printf_str:     .asciz      "array_a[%d] = %d\n"
-notfound_str:   .ascii      "That value does not exist in the array!\n"
+notfound_str:   .asciz      "That value does not exist in the array! \n"
 printf_search:	.asciz		"ENTER A SEARCH VALUE: "
+format_str: 		.asciz 			"%d"
+
